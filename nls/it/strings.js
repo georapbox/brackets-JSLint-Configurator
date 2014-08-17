dc7b0a70f6e71abac2c97484798ea534342da834
Denisov21 <student@alice.it>
2014-08-17 21:40:54 +0200

    Create strings.js
    Translate in Italian language

---
+++ nls/it/strings.js
@@ -0,0 +1,50 @@
+/*global define */
+// Italian - strings
+define({
+'COMMAND_NAME' : 'JSLint Configurator',
+/* DIALOG HEADER */
+'DIALOG_TITLE' : 'JSLint Configurator',
+'CLEAR_OPTIONS' : 'pulisci opzioni',
+/* DIALOG BODY */
+'ASSUME' : 'Assumere',
+'TOLERATE' : 'Sopportare',
+/* JSLINT OPTIONS */
+'BROWSER' : 'un browser',
+'COUCH' : 'CouchDB',
+'DEVEL' : 'console, alert, ...',
+'NODE' : 'Node.js',
+'RHINO' : 'Rhino',
+'PASSFAIL' : 'Ferma al primo errore',
+'ASS' : 'espressioni di assegnamento',
+'BITWISE' : 'operatori bitwise',
+'CLOSURE' : 'Google Closure',
+'CONTINUE' : 'continua',
+'DEBUG' : 'dichiarazioni debugger',
+'EQEQ' : '== e !=',
+'EVIL' : 'eval',
+'FORIN' : 'non filtrato nel',
+'NEWCAP' : 'costruttori in minuscolo',
+'NOMEN' : 'dangling _ in identificazioni',
+'PLUSPLUS' : '++ e --',
+'REGEXP' : '. e [^...] in /RegExp/',
+'UNPARAM' : 'parametri non utilizzati',
+'SLOPPY' : 'dimenticanza \'use strict\' pragma',
+'STUPID' : 'stupidità',
+'SUB' : 'indicizzazione inefficiente',
+'TODO' : 'commenti TODO',
+'VARS' : 'molte dichiarazioni var per funzione',
+'WHITE' : 'disordinato spazio bianco',
+'INDENT' : 'Indentazione',
+'MAXLEN' : 'Massima lunghezza linee',
+'MAXERR' : 'Massimo numero di errori',
+/* DIRECTIVE AREA */
+'DIRECTIVE_TITLE' : 'Direttiva JSLint',
+'HINT' : '<span style="font-weight:bold;">Hint:</span> Inserire sempre la direttiva JSLint nella prima riga del documento.',
+'SELECT' : 'Selezionare',
+'EXTRACT' : 'Estratto di file',
+'JSON_CONVERT' : 'Convertire a JSON',
+/* DIALOG FOOTER */
+'JSLINT_INFO' : 'JSLint documentazione opzioni',
+'DIALOG_OK' : 'Inserire direttiva per documento',
+'DIALOG_CANCEL' : 'Cancella'
+});
